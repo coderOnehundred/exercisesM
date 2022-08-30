2a026d742ad022028225f482b316c87fca0f597a
Manu <manu30mayo@gmail.com>
2022-08-30 19:32:21 +0200

    exercise2

--- if-else-elif.js
+++ if-else-elif.js
@@ -7,5 +7,18 @@ const exercise1 = () => {
     }
 
 }
+// exercise1()
 
-exercise1()
+const exercise2 = (x) => {
+    if (x === true) {
+        console.log("The argument is exactly true");
+    } else {
+        console.log("The argument is not  exactly true");
+    }
+}
+
+// exercise2(true);
+// exercise2(false);
+// exercise2(1);
+// exercise2('');
+// exercise2('test');
