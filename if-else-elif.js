f9e7aebaae64f01b6ed7a8692e4b4060bdf2f7fe
Manu <manu30mayo@gmail.com>
2022-09-02 17:49:27 +0200

    if else exercises

--- if-else-elif.js
+++ if-else-elif.js
@@ -1,11 +1,68 @@
-const exercise1 = () => {
+// const value = true;
 
-    const verdad = true;
+// if (value) {
+//     console.log('code inside if');
+// }
 
-    if (verdad == true) {
-        console.log("The variable is true");
-    }
+// const value = false;
 
-}
+// if (value) {
+//     console.log('code inside if');
+// } else {
+//     console.log('code inside else');
+// }
 
-exercise1()
+// console.log('after if else');
+
+// const value = false;
+// const value2 = true;
+
+// if (value) {
+//     console.log('code inside if');
+// } else {
+//     console.log('code inside else');
+// }
+
+// if (!value) {
+//     console.log('inside second if');
+// }
+
+// console.log('after if else');
+
+// const value = () => false;
+// const value2 = () => true;
+
+// if (value()) {
+//     console.log('code inside if');
+// } else {
+//     console.log('code inside else');
+// }
+
+// if (!value()) {
+//     console.log('inside second if');
+// }
+
+// console.log('after if else');
+
+////////////////////////////////////////////////////////////////////////  you can put whatever you want in the if as long as it results to a value (or returns a value) / transforms
+
+
+// const value = () => false;
+// const value2 = () => true;
+
+// if (value2()) {
+//     console.log("I'm value2");
+// }
+
+// if (value()) {
+//     console.log('code inside if');
+// } else {
+//     console.log('code inside else');
+// }
+
+// if (!value()) {
+//     console.log('inside second if');
+// }
+
+// console.log('after if else');
+
