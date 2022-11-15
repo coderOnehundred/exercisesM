80a60638f1f78ae94d071ac6c73113f0bd72db0e
Manu <manu30mayo@gmail.com>
2022-11-15 13:28:33 +0100

    update 15 nov

--- problem_1.js
+++ problem_1.js
@@ -154,16 +154,108 @@ createEmployee("Joma", "Vice President", 110, 25);
  }
  */
 
-function youAreFired(employeesList, name) {
-  const objWithIdIndex = employeesList.findIndex((obj) => obj.name === name);
-  employeesList.splice(objWithIdIndex, 1);
+function removeEmployee(employeesList, name) {
+  // start a counter at 0 ; go trough every element, check if the name of the element is the one I'm passing as the parameter; when found splice
+  // if employee not found log error and return false;
 
-  return employeesList;
+  let foundIndex = -1;
+  let currentIndex = 0;
+
+  for (let employee of employeesList) {
+    if (employee.name === name) {
+      foundIndex = currentIndex;
+    }
+    currentIndex = currentIndex + 1;
+  }
+
+  if (foundIndex === -1) {
+    console.error(`Employee: ${name} is not found`);
+    return false;
+  }
+  employeesList.splice(foundIndex, 1);
+  // return employeesList;
+
+  //   findIndex(criteriaFunction) {
+  //     let foundIndex = -1;
+  //     let currentIndex = 0;
+  //     for (let element of list) { // It knows about list because you do list.findIndex
+  //         if (criteriaFunction(element)) {
+  //             foundIndex = currentIndex;
+  //             return foundIndex;
+  //         }
+  //         currentIndex = currentIndex + 1;
+  //     }
+  // }
+
+  // (obj) => obj.name === name
+  /*
+      obj => obj.name === name
+
+      obj => {
+        return obj.name === name
+      }
+
+      function someFunc (obj) {
+        return obj.name === name
+      }
+
+  */
+
+  // const objWithIdIndex = employeesList.findIndex((obj) => obj.name === name);
+  // employeesList.splice(objWithIdIndex, 1);
+  // return employeesList;
 }
+removeEmployee(employeesList, "Kate");
 
-youAreFired(employeesList, "Kate");
-youAreFired(employeesList, "Manu");
+function changeRole(employeesList, employeeName, newRole) {
+  // check for newRole
+
+  if (typeof newRole !== "string") {
+    console.error(`newRole: ${newRole} is not a string`);
+    return false;
+  }
+
+  if (newRole.length <= 3) {
+    console.error(`Role: '${newRole}' only has ${newRole.length} characters`);
+    return false;
+  }
+
+  let foundSpecialCharacterInRole = false;
+
+  for (character of newRole) {
+    if (specialCharacters.includes(character)) {
+      console.error(`Role: ${newRole} has the special character: ${character}`);
+      foundSpecialCharacterInRole = true;
+    }
+  }
+
+  if (foundSpecialCharacterInRole) {
+    return false;
+  }
+
+  //
+
+  let foundIndex = -1;
+  let currentIndex = 0;
+
+  for (let employee of employeesList) {
+    if (employee.name === employeeName) {
+      foundIndex = currentIndex;
+    }
+    currentIndex = currentIndex + 1;
+  }
+  if (foundIndex === -1) {
+    console.error(`Employee: ${employeeName} is not found`);
+    return false;
+  }
+
+  employeesList[foundIndex].role = newRole;
+}
 
 // [ { id: 1, name: 'John' }, { id: 3, name: 'Peter' } ]
 console.log("|||||||||||||||||");
+// employeesList[1].role = "new name";
+changeRole(employeesList, "Mihai", "no");
 console.log(employeesList);
+
+// do the checks in role also for new role
